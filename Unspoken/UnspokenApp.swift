723389c11dca68ce808be1828aa39db249347072
Bones <bones7456@gmail.com>
2024-08-11 14:30:23 +0800

    Initial Commit

---
+++ Unspoken/UnspokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnspokenApp.swift
+//  Unspoken
+//
+//  Created by Luyang Li on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct UnspokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
